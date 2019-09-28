c4ccb30c634f27530240764da12fb44d46e0cc49
Andrew Harle <46223597+andrewharle@users.noreply.github.com>
2019-09-28 20:22:09 +0200

    Correct unifi Dockerfile

--- armhf-unifi/docker-build.sh
+++ armhf-unifi/docker-build.sh
@@ -3,16 +3,22 @@
 # fail on error
 set -e
 
+
+#Add repo for openjdk-8-jre-headless from stretch
+echo "deb http://httpredir.debian.org/debian/ stretch main" | tee -a /etc/apt/sources.list
+
+echo "Package: *\nPin: release a=stretch\nPin-Priority: -10\n\nPackage: *\nPin: release a=stable\nPin-Priority: 100" | tee /etc/apt/preferences
+
 apt-get update
 apt-get install -qy --no-install-recommends \
     openjdk-8-jre-headless \
     libcap2-bin
 
 apt-key adv --keyserver hkps://keyserver.ubuntu.com --recv 06E85760C0A52C50 
-echo 'deb http://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
+echo 'deb http://www.ui.com/downloads/unifi/debian stable ubiquiti' | tee /etc/apt/sources.list.d/100-ubnt-unifi.list
 
 apt update
-apt install unifi
+apt install -y unifi
 chown -R unifi:unifi /usr/lib/unifi
 rm -rf /var/lib/apt/lists/*
 
